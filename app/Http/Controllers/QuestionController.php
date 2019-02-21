5bf8494e568cf13406ec283dc3a4b84f74e31f94
bibek12 <sbibek100@gmail.com>
2019-02-21 22:39:12 +0545

    extends form functionality to to updated:

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -67,6 +67,7 @@ class QuestionController extends Controller
     public function edit(Question $question)
     {
         //
+        return view('questions.edit',compact('question'));
     }
 
     /**
@@ -76,9 +77,12 @@ class QuestionController extends Controller
      * @param  \App\Question  $question
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Question $question)
+    public function update(AskQuestionRequest $request, Question $question)
     {
         //
+        $question->update($request->only('title','body'));
+        Session::flash('success','Your Question has been updated');
+        return redirect('/questions');
     }
 
     /**
